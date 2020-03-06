722ee72111bd46a717cc423204387077801bcb5a
Ashish Patel <toaashishpatel@gmail.com>
2020-03-07 01:43:38 +0530

    SystemsManager configuration

--- src/AwsSystemsManagerLabs.Web/Program.cs
+++ src/AwsSystemsManagerLabs.Web/Program.cs
@@ -1,5 +1,6 @@
 #region Imports
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 #endregion
 
@@ -14,9 +15,19 @@ namespace AwsSystemsManagerLabs.Web
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(builder =>
+                {
+                    builder.AddSystemsManager("/myapplication");
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
     }
 }
+
+
+
+#region References
+//https://aws.amazon.com/blogs/developer/net-core-configuration-provider-for-aws-systems-manager/
+#endregion
