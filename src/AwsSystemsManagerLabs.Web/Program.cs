6e0b3324d53e9bc5afa9c70c6278494bc2a24225
Ashish Patel <toaashishpatel@gmail.com>
2020-03-07 15:30:13 +0530

    Advanced configuration

--- src/AwsSystemsManagerLabs.Web/Program.cs
+++ src/AwsSystemsManagerLabs.Web/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using System;
 #endregion
 
 namespace AwsSystemsManagerLabs.Web
@@ -17,7 +18,31 @@ namespace AwsSystemsManagerLabs.Web
             Host.CreateDefaultBuilder(args)
                 .ConfigureAppConfiguration(builder =>
                 {
-                    builder.AddSystemsManager("/myapplication");
+                    builder.AddSystemsManager(configureSource =>
+                    {
+                        // Parameter Store prefix to pull configuration data from.
+                        configureSource.Path = "/myapplication";
+
+                        // Reload configuration data every 5 minutes.
+                        configureSource.ReloadAfter = TimeSpan.FromMinutes(5);
+
+                        // Use custom logic to set AWS credentials and Region. Otherwise, the AWS SDK for .NET's default logic
+                        // will be used find credentials.
+                        //configureSource.AwsOptions = awsOptions;
+
+                        // Configure if the configuration data is optional.
+                        configureSource.Optional = true;
+
+                        configureSource.OnLoadException += exceptionContext =>
+                        {
+                            // Add custom error handling. For example, look at the exceptionContext.Exception and decide
+                            // whether to ignore the error or tell the provider to attempt to reload.
+                        };
+
+                        // Implement custom parameter process, which transforms Parameter Store names into
+                        // names for the .NET Core configuration system.
+                        //configureSource.ParameterProcessor = customerProcess;
+                    });
                 })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
